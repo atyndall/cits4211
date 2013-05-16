15312575174a1d251abe64bf3b64ba0f19fc8183
Nathan Aplin <20965585@student.uwa.edu.au>
2013-05-16 17:24:48 +0800

    Added review function to test the effectiveness of the utility functions

--- utility.py
+++ utility.py
@@ -41,6 +41,38 @@ def utility_function_a(board):
 
     return utility
 
+class variable_alpha:
+    def __init__(self, u_row, u_hole, u_adj_wall, u_adj_floor, u_adj_block):
+        self._UTILITY_OF_ROW = u_row
+        self._UTILITY_HOLE = u_hole
+        self._UTILITY_ADJACENT_TO_WALL = u_adj_wall
+        self._UTILITY_ADJACENT_TO_FLOOR = u_adj_floor
+        self._UTILITY_ADJACENT_TO_BLOCK = u_adj_block
+
+    def __call__(self, board):
+        utility = 0
+        for row in range(0, board.get_num_rows()):
+            utility += self._UTILITY_OF_ROW
+            for column in range(0, board.get_width()):
+                if board.block_at(row, column):
+                    positions = ((row - 1, column),
+                             (row + 1, column),
+                             (row, column - 1),
+                             (row, column + 1))
+                    for pos in positions:
+                        if board.is_wall(pos[1]):
+                            utility += self._UTILITY_ADJACENT_TO_WALL
+                        elif board.is_floor(pos[0]):
+                            utility += self._UTILITY_ADJACENT_TO_FLOOR
+                        elif board.block_at(pos[0], pos[1]):
+                            utility += self._UTILITY_ADJACENT_TO_BLOCK
+                else:
+                    for i in range(row + 1, board.get_num_rows()):
+                        if board.block_at(i, column):
+                            utility += self._UTILITY_HOLE
+                            break;
+        return utility
+
 def test():
     b = board.Board(11)
     
@@ -49,3 +81,5 @@ def test():
     b.apply_move(move.Move(1, 1, 2))
     b.print_grid()
     print(utility_function_a(b))
+    uf = variable_alpha(-100, -80, 10, 1, 1)
+    print(uf(b))
